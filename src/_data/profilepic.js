0e1489bacb50c87e4eb8abe7da2dceb56deadcd0
ihojmanb <ihojmanb@users.noreply.github.com>
2021-09-24 21:17:21 +0000

    deploy: 758648357e1d49a3c8b64ddd96e0a1d4448e5a74

--- src/_data/profilepic.js
+++
@@ -1,7 +0,0 @@
-const md5 = require('md5')
-const axios = require('axios')
-const email = 'ihojmanb@gmail.com'
-const HASH = md5(email)
-module.exports =  () => {
-    return  `https://www.gravatar.com/avatar/${HASH}`
-  };
